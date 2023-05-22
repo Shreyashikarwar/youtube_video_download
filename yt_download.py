e1584e5b7a779ec04d331bc3339ea28857767c5f
Shreyashikarwar <shreyash.sikarwar2012@gmail.com>
2023-05-23 02:01:34 +0530

    first commit

---
+++ yt_download.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+
+def yt_download_link(link):
+    youtube_1 = YouTube(link)
+    print(youtube_1.title)
+
+    # for all the format
+    video = youtube_1.streams.all()
+
+    #for audio only
+    
+    # video = youtube_1.streams.filter(only_audio=True)
+
+
+    vid_list = list(enumerate(video))
+
+    for i in vid_list:
+        print(i)
+
+    strm = int(input("enter : "))
+    video[strm].download()
+    print("successfully")
+
+yt_download_link(link = "https://www.youtube.com/watch?v=XnNzck5-HdQ&list=PLu0W_9lII9ahwLNzab_rVfZ06ZCZYEcNs")
